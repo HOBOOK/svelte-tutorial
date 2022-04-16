dda81a2f4797bcc1ce036dbf00db1710e8289756
ghpark <pkh879@gmail.com>
2022-04-16 15:59:00 +0900

    [ADD] Install Svelte material-ui

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,10 +1,39 @@
+<svelte:head>
+    <!-- 여기는 렌더링 이후에 head 에 추가될 태그들을 정의하는 곳 -->
+	<!-- Fonts -->
+<link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+  />
+
+  <!-- Material Typography -->
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/@material/typography@13.0.0/dist/mdc.typography.css"
+  />
+
+  <!-- SMUI -->
+  <link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+
+  <!-- SMUI Styles -->
+<!-- <link rel="stylesheet" href="/smui.css" media="(prefers-color-scheme: light)" />
+<link
+  rel="stylesheet"
+  href="/smui-dark.css"
+  media="screen and (prefers-color-scheme: dark)"
+/> -->
+</svelte:head>
+
 <script>
-	export let name;
+	import Main from './Main.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Main></Main>
 </main>
 
 <style>
